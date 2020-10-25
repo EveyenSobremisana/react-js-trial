2b9fc71267541fb4f81d592ed311a76c75d3e0e3
EveyenSobremisana <eveyensobremisana@gmail.com>
2020-10-24 17:45:02 -0700

    navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Text1 from "./components/Text1";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
-    return <Text1 />;
+    return (
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    );
   }
 }
 
